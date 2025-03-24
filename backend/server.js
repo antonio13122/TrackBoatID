69645140ad22896f4d82482ae8d0ffaf82326088
Antonio <tonisajbic501@gmail.com>
2025-03-24 15:06:44 +0100

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const multer = require("multer");
+const Tesseract = require("tesseract.js");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("uploads"));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
