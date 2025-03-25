b747551226d6b613883a8ab6221148bb6c299bcc
Antonio <tonisajbic501@gmail.com>
2025-03-25 19:14:51 +0100

    Connected with existing database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+
 const cors = require("cors");
 const multer = require("multer");
 const Tesseract = require("tesseract.js");
 const path = require("path");
+const connectDB = require("./db");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("uploads"));
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+connectDB();
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
